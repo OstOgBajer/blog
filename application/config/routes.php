55090cb7cbdba19792fec487cc5e4a81968bd536
Brianna Kristensen <ostogbajer@gmail.com>
2013-02-04 19:16:58 -0600

    created barebones files to initialize database

--- application/config/routes.php
+++ application/config/routes.php
@@ -38,7 +38,7 @@
 |
 */
 
-$route['default_controller'] = "welcome";
+$route['default_controller'] = "initialize_database";
 $route['404_override'] = '';
 
 
---
+++ application/views/initialize_database.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Initialize Database</title>
+</head>
+<body>
+
+<h1>Initialize Database</h1>
+
+<p><b>Blog Table Exists:</b> <?php echo $blogTableExists ?></p>
+
+</body>
+</html>
